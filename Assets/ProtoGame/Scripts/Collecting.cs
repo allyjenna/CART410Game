73d67b54b62c74453d9f9d84e2345fecaaf0b5eb
Anthony Calderone <anthony.calderone2000@yahoo.com>
2024-11-14 22:50:11 -0500

    game papers collected

--- Assets/ProtoGame/Scripts/Collecting.cs
+++ Assets/ProtoGame/Scripts/Collecting.cs
@@ -2,27 +2,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI; // Include for Slider reference
 
 public class Collecting : MonoBehaviour
 {
     int someInt = 30;
     int papers = 0;
     public AudioSource paperPickUp;
+
+    // Reference to the Slider UI component
+    public Slider paperProgressBar;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (paperProgressBar != null)
+        {
+            paperProgressBar.maxValue = 3; // Total number of papers to collect
+            paperProgressBar.value = 0; // Starting point of the progress bar
+        }
+        else
+        {
+            Debug.LogError("Paper Progress Bar is not assigned in the Inspector.");
+        }
     }
 
     public void OnTriggerEnter(Collider Col)
     {
         if(Col.gameObject.tag == "Paper")
         {   
-            Col.gameObject.SetActive(false);
-            papers++;
-            paperPickUp.Play();
+            Col.gameObject.SetActive(false);  // Deactivate the paper once collected
+            papers++;  // Increase the paper count
+            paperPickUp.Play();  // Play pickup sound
 
+            // Update the slider value based on collected papers
+            if (paperProgressBar != null)
+            {
+                paperProgressBar.value = papers;  // Update slider to reflect the collected papers
+            }
         }
+        
+        // Check if 3 papers are collected and load the "end" scene
         if(papers == 3)
         {
             SceneManager.LoadScene("end");
@@ -31,9 +51,10 @@ public class Collecting : MonoBehaviour
         }
     }
 
-    void OnGUI(){
-        GUI.skin.label.fontSize = someInt;
-        GUI.Label (new Rect (0, 400, 500, 100), "Paper's collected :" + papers + "/3");
-    
-    }
+    // void OnGUI()
+    // {
+    //     // Display the number of collected papers
+    //     GUI.skin.label.fontSize = someInt;
+    //     GUI.Label(new Rect(0, 400, 500, 100), "Papers collected: " + papers + "/3");
+    // }
 }
